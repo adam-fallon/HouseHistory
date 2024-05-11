04f37d5fa5fa7b601cb6422d0af79fdb820eb630
Adam Fallon <adam@adamfallon.com>
2024-05-11 20:09:39 +0100

    Add Login

--- API/Controllers/Auth/Auth.cs
+++ API/Controllers/Auth/Auth.cs
@@ -10,7 +10,14 @@ namespace HouseHistory.Routes.Auth
         {
             return builder.UseEndpoints(endpoints =>
             {
-                endpoints.MapPost($"/api/v1/auth/register", async (ISupabaseService supabaseService, SignUpRequest signUpRequest) => { });
+                endpoints.MapPost($"/api/v1/auth/login", async (ISupabaseService supabaseService, LoginRequest loginRequest) =>
+                {
+                    var supabase = builder.ApplicationServices.GetRequiredService<ISupabaseService>();
+                    var client = await supabase.GetClient();
+                    var session = await client.Auth.SignIn(loginRequest.Email, loginRequest.Password);
+                    return session;
+                });
+
                 endpoints.MapPost($"/api/v1/auth/signup", async (ISupabaseService supabaseService, SignUpRequest signUpRequest) =>
                 {
                     var supabase = builder.ApplicationServices.GetRequiredService<ISupabaseService>();
